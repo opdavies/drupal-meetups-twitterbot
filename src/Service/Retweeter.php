6b8c05f30ad9db4f85cca46c0e8249a255607de0
Oliver Davies <oliver@oliverdavies.uk>
2019-01-10 23:05:24 +0000

    Inject the Codebird service

--- src/Service/Retweeter.php
+++ src/Service/Retweeter.php
@@ -2,6 +2,27 @@
 
 namespace App\Service;
 
+use Codebird\Codebird;
+
 class Retweeter
 {
+
+    private $codebird;
+
+    public function __construct(Codebird $codebird)
+    {
+        $codebird::setConsumerKey(
+            getenv('TWITTER_CONSUMER_KEY'),
+            getenv('TWITTER_CONSUMER_SECRET')
+        );
+
+        $codebird = $codebird::getInstance();
+
+        $codebird->setToken(
+            getenv('TWITTER_ACCESS_TOKEN'),
+            getenv('TWITTER_ACCESS_SECRET')
+        );
+
+        $this->codebird = $codebird;
+    }
 }
