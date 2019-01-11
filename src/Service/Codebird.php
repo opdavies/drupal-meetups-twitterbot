e1e4c2e9d6d003f527c880f0f3e6995d2e55c2e7
Oliver Davies <oliver@oliverdavies.uk>
2019-01-11 00:15:37 +0000

    Add Codebird service

---
+++ src/Service/Codebird.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+class Codebird
+{
+
+    /**
+     * @var \Codebird\Codebird
+     */
+    private $codebird;
+
+    public function __construct(\Codebird\Codebird $codebird)
+    {
+        $codebird::setConsumerKey(
+            getenv('TWITTER_CONSUMER_KEY'),
+            getenv('TWITTER_CONSUMER_SECRET')
+        );
+
+        $codebird = $codebird::getInstance();
+
+        $codebird->setToken(
+            getenv('TWITTER_ACCESS_TOKEN'),
+            getenv('TWITTER_ACCESS_SECRET')
+        );
+
+        $this->codebird = $codebird;
+    }
+
+    public function get(): \Codebird\Codebird
+    {
+        return $this->codebird;
+    }
+
+}
